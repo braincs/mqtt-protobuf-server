b3ec2074c72bf6053b8d20f2f41f3cc4379ae160
shuai <changshuai@megvii.com>
2019-05-16 21:32:47 +0800

    mqtt-protobuf-server v0.0.1
    Feat
     - init project

---
+++ src/main/java/com/braincs/mqttprotobufserver/MqttProtobufServerApplication.java
@@ -0,0 +1,13 @@
+package com.braincs.mqttprotobufserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MqttProtobufServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MqttProtobufServerApplication.class, args);
+    }
+
+}
